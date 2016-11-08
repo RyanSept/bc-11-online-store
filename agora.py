147159627ca8e9e844415325f8ad605de564d23e
Ryan Marvin <marvinryan@ymail.com>
2016-11-08 15:34:10 +0300

    Added templates

---
+++ agora.py
@@ -0,0 +1,148 @@
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+abort, render_template, flash
+
+app = Flask(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'agora.db'),
+    DEBUG=True,
+    SECRET_KEY='@agorakey#1',
+    USERNAME='admin',
+    PASSWORD='password'
+))
+app.config.from_envvar('AGORA_SETTINGS', silent=True)
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+	
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def check_password(username, password):
+	res = g.db.execute('SELECT * FROM users WHERE user_name=? AND user_password=?',\
+    	[username, password]).fetchall()
+	if len(res)>0:
+		return True
+	else:
+		return False
+
+def user_exists(username):
+	res = g.db.execute('SELECT * FROM users WHERE user_name=?',[username]).fetchall()
+	if len(res)>0:
+		return True
+	else:
+		return False
+def shop_exists(shop_name):
+	res = g.db.execute('SELECT * FROM shops WHERE shop_name=?',[shop_name]).fetchall()
+	if len(res)>0:
+		return True
+	else:
+		return False
+
+def get_current_user_id():
+	res = g.db.execute('SELECT user_id FROM users WHERE user_name=?',
+		[app.config['USERNAME']]).fetchall()
+
+	return res[0][0]
+
+
+@app.route('/')
+def homepage():
+	flash(str(get_current_user_id()))
+	return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+    	res = check_password(request.form['username'],request.form['password'])
+    	if res:
+    		app.config['USERNAME'] = request.form['username']
+        if not res:
+        	error = 'Invalid username or password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('homepage'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('homepage'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    """Registers the user."""
+    if session.get('logged_in'):
+        return redirect(url_for('homepage'))
+    error = None
+    if request.method == 'POST':
+        if not request.form['username']:
+            error = 'You have to enter a username'
+        elif not request.form['email'] or \
+                '@' not in request.form['email']:
+            error = 'You have to enter a valid email address'
+        elif not request.form['password']:
+            error = 'You have to enter a password'
+        elif request.form['password'] != request.form['password2']:
+            error = 'The two passwords do not match'
+        elif user_exists(request.form['username']):
+            error = 'The username is already taken'
+        if not error:
+            g.db.execute('''insert into users (
+              username, user_email, user_password) values (?, ?, ?)''',
+              [request.form['username'], request.form['email'],
+               request.form['password']])
+            g.db.commit()
+            flash('You were successfully registered and can login now')
+            return redirect(url_for('login'))
+    return render_template('signup.html', error=error)
+
+@app.route('/create-store', methods=['GET','POST'])
+def create_store():
+	if not session.get('logged_in'):
+		return redirect(url_for('homepage'))
+	error = None
+	if request.method == 'POST':
+		form = request.form
+
+		if not form['shopname']:
+			error = 'You have to enter a shop name'
+		elif user_exists(form['shopname']):
+			error = 'A shop with this name already exists.'
+		elif not form['shoplocation']:
+			error = 'You did not enter the location of the shop'
+		elif not form['shopdescription']:
+			form['shopdescription'] = ''
+
+		else:
+			g.db.execute('INSERT INTO shop (shop_name, shop_desc, shop_location,shop_url) values(?,?,?,?)',\
+				[form['shopname'],form['shopdescription'],form['shoplocation'],form['shopname'].replace(' ','-')])
+
+			shop_id = g.db.execute('SELECT * FROM shop ORDER BY shop_id DESC LIMIT 1').fetchall()[0][0]
+			
+			g.db.execute('INSERT INTO users_shop(shop_id,user_id) values(?,?)',[shop_id, get_current_user_id()])
+			g.db.commit()
+
+		flash('Congratulations on creating your first shop!')
+	return render_template('create-store.html',error=error)
+
+@app.route('/<shopname>')
+def view_shop(shop_name):
+	g.db.execute('SELECT * FROM shops WHERE shop_name=?')
+
+if __name__ == '__main__':
+	app.run()
--- schema.sql
+++ schema.sql
@@ -1,20 +1,96 @@
-CREATE TABLE shop (
-    shop_id integer primary key autoincrement,
-    shop_name varchar,
-    shop_desc text,
-    shop_location varchar,
-    shop_url varchar,
-);
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 08, 2016 at 08:01 AM
+-- Server version: 5.5.49-log
+-- PHP Version: 7.0.6
 
-CREATE TABLE product (
-    product_id integer primary key autoincrement,
-    product_title varchar,
-    product_desc text,
-    product_price float, 
-    product_image varchar,
-);
 
-CREATE TABLE shopproduct (
-    shop_id FOREIGN KEY (shop_ID) REFERENCES shop(shop_id),
-    product_id FOREIGN KEY (product_id) REFERENCES product(product_id),
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `agora`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `product_id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  `product_title` varchar(255) NOT NULL,
+  `product_desc` text NOT NULL,
+  `product_price` float NOT NULL,
+  `product_image` varchar(255) NOT NULL,
+  `creation_date` date NOT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop`
+--
+
+CREATE TABLE IF NOT EXISTS `shop` (
+  `shop_id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  `shop_name` varchar(255) NOT NULL,
+  `shop_desc` text NOT NULL,
+  `shop_location` varchar(255) NOT NULL,
+  `shop_url` varchar(255) NOT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop_products`
+--
+
+CREATE TABLE IF NOT EXISTS `shop_products` (
+  `shop_id` integer NOT NULL,
+  `product_id` integer NOT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_shop`
+--
+
+CREATE TABLE IF NOT EXISTS `users_shop` (
+  `user_id` integer NOT NULL,
+  `shop_id` integer NOT NULL
+) ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION ENGINE=InnoDB DEFAULT CHARSET=latin1*/;
